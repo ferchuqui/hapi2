8b63ae7438299df0be2b611341d7d86fa69ae089
ferchuqui <luisfernandofarji@hotmail.com>
2019-08-08 12:17:04 -0300

    Static Content

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
 "use strict"
 const Hapi = require('@hapi/hapi')
+const inert = require ('inert')
+const path = require ('path')
 const server = Hapi.server({
   port: process.env.PORT || 3000,
-  host: 'localhost'
+  host: 'localhost',
+    routes:{
+      files:{
+        relativeTo: path.join(__dirname, 'public')
+      }
+    }
 })
 async function init(){
-  server.route({
-    method:'GET',
-    path:'/',
-    handler: (req, h)=>{
-      return 'hola mundo...'
-    }
-  })
+
+
   try {
+    await server.register(inert)
+    server.route({
+      method:'GET',
+      path:'/home',
+      handler: (req, h)=>{
+        return h.file('index.html')
+      }
+    })
+    server.route({
+      method:'GET',
+      path:'/{param*}',
+      handler: {
+        directory:{
+          path: '.',
+          index: ['index.html']
+        }
+      }
+    })
     await server.start()
+
   } catch (error) {
     console.error(error)
     process.exit(1)
