60e5162044b03755f6da649afa6876dbb4548d05
ferchuqui <luisfernandofarji@hotmail.com>
2019-08-08 10:16:18 -0300

    First Server

---
+++ index.js
@@ -0,0 +1,23 @@
+"use strict"
+const Hapi = require('@hapi/hapi')
+const server = Hapi.server({
+  port: process.env.PORT || 3000,
+  host: 'localhost'
+})
+async function init(){
+  server.route({
+    method:'GET',
+    path:'/',
+    handler: (req, h)=>{
+      return 'hola mundo...'
+    }
+  })
+  try {
+    await server.start()
+  } catch (error) {
+    console.error(error)
+    process.exit(1)
+  }
+  console.log(`Servidor lanzado en: ${server.info.uri}`)
+}
+init()
