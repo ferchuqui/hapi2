1c74d2ba22300dca9d3ebdcdcadc30e901019a4b
ferchuqui <luisfernandofarji@hotmail.com>
2019-08-09 15:54:17 -0300

    Good Practices Chapter 9

--- index.js
+++ index.js
@@ -1,80 +1,46 @@
-"use strict"
+'use strict'
+
 const Hapi = require('@hapi/hapi')
-const handlebars = require('handlebars')
+const handlerbars = require('handlebars')
+const inert = require('inert')
+const path = require('path')
+const routes = require('./routes')
 const vision = require('vision')
-const inert = require ('inert')
-const path = require ('path')
+
 const server = Hapi.server({
   port: process.env.PORT || 3000,
   host: 'localhost',
-    routes:{
-      files:{
-        relativeTo: path.join(__dirname, 'public')
-      }
+  routes: {
+    files: {
+      relativeTo: path.join(__dirname, 'public')
     }
+  }
 })
-async function init(){
 
-
+async function init () {
   try {
     await server.register(inert)
     await server.register(vision)
+
     server.views({
       engines: {
-        hbs: handlebars
+        hbs: handlerbars
       },
       relativeTo: __dirname,
       path: 'views',
       layout: true,
-      layoutPath : 'views'
-    })
-    
-    server.route({
-      method: 'GET',
-      path: '/',
-      handler: (req, h) => {
-        return h.view('index', {
-          title: 'home'
-        })
-      }
+      layoutPath: 'views'
     })
 
-    server.route({
-      method:'GET',
-      path:'/{param*}',
-      handler: {
-        directory:{
-          path: '.',
-          index: ['index.html']
-        }
-      }
-    })
+    server.route(routes)
 
-    server.route({
-      method: 'GET',
-      path: '/register',
-      handler: (req, h) => {
-        return h.view('register', {
-          title: 'Registro'
-        })
-      }
-    })
-
-    server.route({
-      method: 'POST',
-      path: '/create-user',
-      handler: (req, h) => {
-        console.log(req.payload)
-        return 'usuario creado'
-      }
-    })
-    
     await server.start()
-
   } catch (error) {
     console.error(error)
     process.exit(1)
   }
+
   console.log(`Servidor lanzado en: ${server.info.uri}`)
 }
+
 init()
